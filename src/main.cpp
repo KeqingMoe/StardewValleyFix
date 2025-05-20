4d517e1c3bf8463b8a9cbe04299055a626da315a
keqingmoe <me@keqing.moe>
2025-05-20 17:34:29 +0800

    重构

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,40 @@
-#include <homedir.hpp>
-#include <find.hpp>
-#include <fix.hpp>
+import std;
 
-using namespace std;
+namespace fs = std::filesystem;
 
-int main()
+auto main() -> int
 {
-    auto home_path=homedir();
-    auto saves=find(home_path);
-    for(auto&& save:saves){
-        fix(save);
+    auto home_path = std::string{std::getenv("USERPROFILE")};
+
+    auto reg_find = std::regex{R"(^(?!SaveGameInfo)(?!.*(_old|\.vdf|\.bak)$).+$)"};
+    auto path     = std::format(R"({}\AppData\Roaming\StardewValley\Saves)", home_path);
+    auto saves    = std::vector<fs::path>{};
+    for (auto& dir_entry : fs::recursive_directory_iterator(path)) {
+        if (!dir_entry.is_regular_file()) {
+            continue;
+        }
+        if (!regex_match(dir_entry.path().filename().string(), reg_find)) {
+            continue;
+        }
+        saves.emplace_back(dir_entry.path());
+    }
+
+    for (auto&& path : saves) {
+        auto reg_fix  = std::regex{R"(<userID>\d*</userID>)"};
+        auto bak_path = format("{}.bak", path.string());
+        fs::copy_file(path, bak_path, fs::copy_options::overwrite_existing);
+
+        using is_it = std::istreambuf_iterator<char>;
+        using os_it = std::ostreambuf_iterator<char>;
+
+        auto fin = std::ifstream{bak_path};
+        auto beg = is_it{fin};
+        auto end = is_it{};
+        auto ctx = std::string{beg, end};
+
+        auto fout = std::ofstream{path};
+        auto it   = os_it{fout};
+
+        std::regex_replace(it, ctx.begin(), ctx.end(), reg_fix, "<userID />");
     }
 }
