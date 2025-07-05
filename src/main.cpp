b51d28725321bd322aa195aea702c7995b2265d6
keqingmoe <me@keqing.moe>
2025-07-05 23:13:04 +0800

    优化交互

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,10 @@ namespace fs = std::filesystem;
 auto main() -> int
 {
     auto reg_find = std::regex{R"(^(?!SaveGameInfo)(?!.*(_old|\.vdf|\.bak)$).+$)"};
-    auto path  = saves_path();
+    auto path     = saves_path();
+
+    std::println("正在扫描存档所在目录：{}", path);
+
     auto saves = std::vector<fs::path>{};
     for (auto& dir_entry : fs::recursive_directory_iterator(path)) {
         if (!dir_entry.is_regular_file()) {
@@ -30,10 +33,25 @@ auto main() -> int
         saves.emplace_back(dir_entry.path());
     }
 
-    for (auto&& path : saves) {
+    std::println(
+        "扫描完毕，一共有 {} 个存档，接下来会逐个询问是否需要修复，若需要则输入 y 或 Y，输入其他内容则不修复。",
+        saves.size());
+    for (auto&& save : saves) {
+        std::println();
+
+        auto rel = save.lexically_relative(path).string();
+        std::println("是否需要修复 {}", rel);
+
+        auto op = std::string{};
+        std::getline(std::cin, op);
+        if (op != "y" && op != "Y") {
+            std::println("跳过 {}", rel);
+            continue;
+        }
+
         auto reg_fix  = std::regex{R"(<userID>\d*</userID>)"};
-        auto bak_path = format("{}.bak", path.string());
-        fs::copy_file(path, bak_path, fs::copy_options::overwrite_existing);
+        auto bak_path = format("{}.bak", save.string());
+        fs::copy_file(save, bak_path, fs::copy_options::overwrite_existing);
 
         using is_it = std::istreambuf_iterator<char>;
         using os_it = std::ostreambuf_iterator<char>;
@@ -47,5 +65,7 @@ auto main() -> int
         auto it   = os_it{fout};
 
         std::regex_replace(it, ctx.begin(), ctx.end(), reg_fix, "<userID />");
+
+        std::println("已修复 {0} 并将原存档备份至 {0}.bak", rel);
     }
 }
